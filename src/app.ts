e5f45105d806d9b5f3d9faf798f13f0e24a914f2
Zyriab <flacodevworks@gmail.com>
2022-10-11 14:11:10 +0200

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import verifyToken from './utils/verifyToken.utils';
+import { RequestBody, ResponseBody } from './definitions/custom';
+import 'dotenv/config';
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+
+const app = express();
+const router = express.Router();
+
+if (!IS_DEV) app.use(helmet());
+app.use(bodyParser.json());
+
+// eslint-disable-next-line consistent-return
+app.use((req: RequestBody, res: ResponseBody<any>, next: any) => {
+  // CORS is taken care of in AWS Lambda
+  if (IS_DEV) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  }
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use('/', router);
+
+router.post('/', async (req, res) => {
+  const success = await verifyToken(req.body.token);
+
+  if (success) {
+    res.status(200);
+    return;
+  }
+
+  res.status(400);
+  res.json({ message: 'Invalid token' });
+});
+
+export default app;
